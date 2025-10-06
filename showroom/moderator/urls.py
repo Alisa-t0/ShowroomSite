8b22d83a924063816dc649e041bd366e48141519
Alisa-t0 <alisa.todorash09@gmail.com>
2025-10-06 13:27:35 +0300

    added jinja, main page, workers, cars, sales lists pages for moderator, added css

--- showroom/moderator/urls.py
+++ showroom/moderator/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.show_moderator_main_page, name='moderator_main_page'),
+    path('workers/', views.show_workers_list, name='workers_list'),
+    path('cars/', views.show_cars_list, name='cars_list'),
+    path('sales/', views.show_sales_list, name='sales_list'),
 ]
--- showroom/moderator/views.py
+++ showroom/moderator/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from main.models import Worker, Car, Sale
 # Create your views here.
 def show_moderator_main_page(request):
     return render(request, 'moderator/moderator_main_page.html')
+
+def show_workers_list(request):
+    all_workers = Worker.objects.all()
+    return render(request, 'moderator/workers/workers_list.html', {'all_workers': all_workers})
+
+def show_cars_list(request):
+    all_cars = Car.objects.all()
+    return render(request, 'moderator/cars/cars_list.html', {'all_cars': all_cars})
+
+def show_sales_list(request):
+    all_sales = Sale.objects.all()
+    return render(request, 'moderator/sales/sales_list.html', {'all_sales': all_sales})
